a6068f9bef7a5a3a7378cf3e7fb45bbd4d63793c
Tyler Long <tyler4long@gmail.com>
2021-08-28 13:09:41 -0700

    Add test case for EventEmitter Limit

---
+++ test/eventEmitterLimit.spec.ts
@@ -0,0 +1,12 @@
+import EventEmitter from 'events';
+
+describe('EventEmitter Limit', () => {
+  test('default', () => {
+    const eventEmitter = new EventEmitter();
+    for (let i = 0; i < 10; i++) {
+      eventEmitter.on('event', () => {});
+    }
+    expect(eventEmitter.listenerCount('event')).toBe(10);
+    expect(eventEmitter.listenerCount('')).toBe(0);
+  });
+});
