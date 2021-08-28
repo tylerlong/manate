14963f0908759e8f4d61a321e3c2128a3a3118db
Tyler Long <tyler4long@gmail.com>
2021-08-27 23:02:59 -0700

    Release 0.2.0

--- README.md
+++ README.md
@@ -12,6 +12,44 @@ Unlike `useState`, which only works with React functions; `useProxy` mainly work
 ## Installation
 
 ```
-yarn add use-proxy
+yarn add @tylerlong/use-proxy
 ```
 
+
+## Usage
+
+```ts
+import {useProxy} from '@tylerlong/use-proxy';
+import {Component} from '@tylerlong/use-proxy/build/react';
+
+class Store {
+  count = 0;
+  increase() {
+    this.count += 1;
+  }
+}
+const [store] = useProxy(new Store());
+
+type AppProps = {
+  store: Store;
+};
+class App extends Component<AppProps> {
+  render() {
+    const store = this.props.store;
+    return (
+      <div>
+        <span>{store.count}</span>
+        <button onClick={() => store.increase()}>+</button>
+      </div>
+    );
+  }
+}
+```
+
+## For maintainers
+
+### How to publish
+
+```
+npm publish --access=public
+```
--- test/react.spec.tsx
+++ test/react.spec.tsx
@@ -5,12 +5,9 @@ import {Component} from '../src/react';
 import {useProxy} from '../src';
 
 class Store {
-  number = 0;
-  decrease() {
-    this.number -= 1;
-  }
+  count = 0;
   increase() {
-    this.number += 1;
+    this.count += 1;
   }
 }
 
@@ -25,11 +22,10 @@ const renderHistory: number[] = [];
 class App extends Component<AppProps> {
   render() {
     const store = this.props.store;
-    renderHistory.push(store.number);
+    renderHistory.push(store.count);
     return (
       <div>
-        <button onClick={() => store.decrease()}>-</button>
-        <span>{store.number}</span>
+        <span>{store.count}</span>
         <button onClick={() => store.increase()}>+</button>
       </div>
     );
@@ -40,14 +36,14 @@ describe('React', () => {
   test('default', async () => {
     const renderer = TestRenderer.create(<App store={store} />);
     const minusButton = renderer.root.find(
-      el => el.type === 'button' && el.children && el.children[0] === '-'
+      el => el.type === 'button' && el.children && el.children[0] === '+'
     );
     minusButton.props.onClick();
     minusButton.props.onClick();
     minusButton.props.onClick();
     const span = renderer.root.find(el => el.type === 'span');
-    expect(store.number).toEqual(parseInt(span.children[0] as string));
-    expect(store.number).toBe(-3);
-    expect(renderHistory).toEqual([0, -1, -2, -3]);
+    expect(store.count).toEqual(parseInt(span.children[0] as string));
+    expect(store.count).toBe(3);
+    expect(renderHistory).toEqual([0, 1, 2, 3]);
   });
 });
