db9f5771c7db4d3db715a40ddf8826e249cb8e10
Tyler Long <tyler4long@gmail.com>
2021-08-28 16:28:10 -0700

    Add listenerCount test case

--- test/listenerCount.spec.ts
+++ test/listenerCount.spec.ts
@@ -11,17 +11,16 @@ describe('Listener count', () => {
     });
 
     expect(getEmitter(proxy)?.listenerCount('event')).toBe(0);
-    console.log(getEmitter(proxy.a));
-    // expect(getEmitter(proxy.a)?.listenerCount('event')).toBe(1);
-    // expect(getEmitter(proxy.a.b)?.listenerCount('event')).toBe(1);
+    expect(getEmitter(proxy.a)?.listenerCount('event')).toBe(1);
+    expect(getEmitter(proxy.a.b)?.listenerCount('event')).toBe(1);
     expect(getEmitter(proxy.a.b.c)?.listenerCount('event')).toBe(undefined);
     const temp = proxy.a.b;
     proxy.a.b = temp;
     proxy.a.b = temp;
     proxy.a.b = temp;
     expect(getEmitter(proxy)?.listenerCount('event')).toBe(0);
-    // expect(getEmitter(proxy.a)?.listenerCount('event')).toBe(1);
-    // expect(getEmitter(proxy.a.b)?.listenerCount('event')).toBe(1);
+    expect(getEmitter(proxy.a)?.listenerCount('event')).toBe(1);
+    expect(getEmitter(proxy.a.b)?.listenerCount('event')).toBe(1);
     expect(getEmitter(proxy.a.b.c)?.listenerCount('event')).toBe(undefined);
   });
 });
