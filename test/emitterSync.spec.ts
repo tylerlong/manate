031a34e6c8bf1ff0d92fb2546ef4496039b6b151
Tyler Long <tyler4long@gmail.com>
2021-08-30 22:36:21 -0700

    Add a test case about emitter sync

---
+++ test/emitterSync.spec.ts
@@ -0,0 +1,19 @@
+import {EventEmitter} from 'events';
+
+describe('emitter sync', () => {
+  test('default', async () => {
+    const emitter = new EventEmitter();
+    const list: number[] = [];
+    emitter.on('event', (number: number) => {
+      list.push(number);
+    });
+    emitter.emit('event', 0);
+    emitter.emit('event', 1);
+    emitter.emit('event', 2);
+    list.push(100);
+    emitter.emit('event', 3);
+    emitter.emit('event', 4);
+    emitter.emit('event', 5);
+    expect(list).toEqual([0, 1, 2, 100, 3, 4, 5]);
+  });
+});
