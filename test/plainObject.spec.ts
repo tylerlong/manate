e7e4092e3b87f0e26a4f78b50d15b04f59c8585a
Tyler Long <tyler4long@gmail.com>
2023-01-24 13:00:06 -0800

    Add one more plain objects test

--- test/plainObject.spec.ts
+++ test/plainObject.spec.ts
@@ -15,4 +15,19 @@ describe('plain objects', () => {
     });
     expect(store.config.__emitter__).toBeUndefined();
   });
+
+  test('another way', () => {
+    class Store {
+      config = {};
+    }
+    const store = useProxy(new Store());
+    store.config = {
+      someString: 'sss',
+      someArr: [],
+    };
+    expect(store.__emitter__).toBeDefined();
+    expect((store.config as any).__emitter__).toBeDefined();
+    const plainObject = JSON.parse(JSON.stringify(store.config));
+    expect(plainObject.__emitter__).toBeUndefined();
+  });
 });
