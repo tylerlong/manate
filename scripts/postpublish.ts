b09b00ce153d9884d728ad10591972cddac3f474
Tyler Liu <tyler4long@gmail.com>
2023-11-24 18:04:00 -0800

    Fix publishing

--- scripts/postpublish.ts
+++ scripts/postpublish.ts
@@ -2,12 +2,6 @@ import { run } from 'shell-commands';
 
 const main = async () => {
   await run(`
-    tsc
-    cp lib/* .
-    rm -rf lib
-  `);
-  await run('npm publish');
-  await run(`
     rm index.d.ts
     rm index.js
     rm models.d.ts
