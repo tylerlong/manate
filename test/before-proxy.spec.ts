9fddf058cd300d4c85665e222655095755ab3cdd
Tyler Liu <tyler4long@gmail.com>
2024-07-16 09:50:12 -0700

    Test before and after proxy

---
+++ test/before-proxy.spec.ts
@@ -0,0 +1,65 @@
+import { manage } from '../src';
+import type { ManateEvent } from '../src/models';
+
+describe('before proxy', () => {
+  test("Updagte an object before it's proxied", () => {
+    class A {
+      public b: B;
+    }
+    class B {
+      public c = 0;
+    }
+
+    const a = new A();
+    const ma = manage(a);
+    const events: string[] = [];
+    ma.$e.on((event: ManateEvent) => {
+      events.push(`${event.name}-${event.pathString}`);
+    });
+    const b = new B();
+    ma.b = b;
+    b.c = 1;
+    expect(events).toEqual(['set-b']);
+  });
+
+  test("Updagte an object after it's proxied", () => {
+    class A {
+      public b: B;
+    }
+    class B {
+      public c = 0;
+    }
+
+    const a = new A();
+    const ma = manage(a);
+    const events: string[] = [];
+    ma.$e.on((event: ManateEvent) => {
+      events.push(`${event.name}-${event.pathString}`);
+    });
+    const b = new B();
+    ma.b = b;
+    ma.b.c = 1;
+    expect(events).toEqual(['set-b', 'get-b', 'set-b+c']);
+  });
+
+  test("Updagte an object after it's proxied - 2", () => {
+    class A {
+      public b: B;
+    }
+    class B {
+      public c = 0;
+    }
+
+    const a = new A();
+    const ma = manage(a);
+    const events: string[] = [];
+    ma.$e.on((event: ManateEvent) => {
+      events.push(`${event.name}-${event.pathString}`);
+    });
+    const b = new B();
+    ma.b = b;
+    const mb = ma.b;
+    mb.c = 1;
+    expect(events).toEqual(['set-b', 'get-b', 'set-b+c']);
+  });
+});
