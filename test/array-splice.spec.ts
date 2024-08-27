786c115f2a433651d8d70d88b0d7378cdb325b56
Tyler Liu <tyler4long@gmail.com>
2024-08-27 11:44:01 -0700

    Add test case array-splice

---
+++ test/array-splice.spec.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from 'vitest';
+
+import { manage } from '../src';
+import type { ManateEvent } from '../src/models';
+
+describe('array splice', () => {
+  test('default', () => {
+    const arr = manage([1, 2, 3, 4, 5]);
+    const events: string[] = [];
+    arr.$e.on((event: ManateEvent) => {
+      events.push(event.toString());
+    });
+    arr.splice(2, 1);
+    expect(events).toEqual(['get: length', 'get: 2', 'get: 3', 'set: 2', 'get: 4', 'set: 3', 'set: length']);
+  });
+});
