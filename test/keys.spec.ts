1222b7a9cd8d09ece217d287d5ef177f4ebccc46
Tyler Liu <tyler4long@gmail.com>
2024-09-19 11:13:16 -0700

    Add test case for values

--- test/keys.spec.ts
+++ test/keys.spec.ts
@@ -1,6 +1,7 @@
 import { describe, expect, test } from 'vitest';
 
 import { autoRun, manage } from '../src';
+import type { ManateEvent } from '../src/models';
 
 describe('keys', () => {
   test('default', () => {
@@ -17,5 +18,37 @@ describe('keys', () => {
     store.monsters['a'] = 1;
     store.monsters['b'] = 2;
     expect(count).toBe(3);
+  });
+
+  test('values', () => {
+    class Store {
+      public monsters: { [key: string]: number } = {};
+    }
+    const store = manage(new Store());
+    let count = 0;
+    const { start } = autoRun(store, () => {
+      expect(Object.values(store.monsters).length).toBe(count);
+      count += 1;
+    });
+    start();
+    store.monsters['a'] = 1;
+    store.monsters['b'] = 2;
+    expect(count).toBe(3);
+  });
+
+  test('values low level', () => {
+    class Store {
+      public monsters: { [key: string]: number } = {};
+    }
+    const store = manage(new Store());
+    const events: { name: string; paths: PropertyKey[] }[] = [];
+    store.$e.on((event: ManateEvent) => {
+      events.push({ name: event.name, paths: event.paths });
+    });
+    expect(Object.values(store.monsters)).toEqual([]); // trigger events
+    expect(events).toEqual([
+      { name: 'get', paths: ['monsters'] },
+      { name: 'keys', paths: ['monsters'] },
+    ]);
   });
 });
