fd968cfe55ce0e525fa4c45f820f3d0cfe29b580
Tyler Liu <tyler4long@gmail.com>
2024-09-19 17:49:26 -0700

    Add a test case for strict mode

---
+++ test/react-strict-mode.spec.tsx
@@ -0,0 +1,67 @@
+// @vitest-environment jsdom
+import { describe, expect, test } from 'vitest';
+import { render, cleanup, act } from '@testing-library/react';
+import React, { StrictMode } from 'react';
+
+import { auto } from '../src/react';
+import { manage } from '../src';
+
+describe('React strict mode', () => {
+  test('Update children props', async () => {
+    let id = 0;
+    class Bullet {
+      public id: number;
+      public speed = 10;
+      public constructor() {
+        this.id = id++;
+      }
+    }
+    class Store {
+      public bullets: { [key: number]: Bullet } = {};
+    }
+
+    let bulletCount = 0;
+    const BulletComponent = auto((props: { bullet: Bullet }) => {
+      bulletCount += 1;
+      const { bullet } = props;
+      return bullet.speed;
+    });
+
+    let appCount = 0;
+    const App = auto((props: { store: Store }) => {
+      appCount += 1;
+      const { store } = props;
+      return Object.values(store.bullets).map((bullet) => <BulletComponent key={bullet.id} bullet={bullet} />);
+    });
+
+    const store = manage(new Store());
+    store.bullets[0] = new Bullet();
+
+    render(
+      <StrictMode>
+        <App store={store} />
+      </StrictMode>,
+    );
+    expect(appCount).toBe(2);
+    expect(bulletCount).toBe(2);
+    act(() => {
+      store.bullets[0].speed = 20;
+    });
+    expect(appCount).toBe(2);
+    expect(bulletCount).toBe(4);
+    act(() => {
+      store.bullets[0].speed = 30;
+    });
+    expect(appCount).toBe(2);
+    expect(bulletCount).toBe(6);
+    act(() => {
+      store.bullets[1] = new Bullet();
+    });
+    expect(appCount).toBe(4);
+    expect(bulletCount).toBe(8);
+    cleanup();
+
+    // compare to test/react-re-render.spec.tsx
+    // this tese all count are doubled because of strict mode
+  });
+});
