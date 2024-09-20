9eb30fafca0225f01e43d19f4fc844560a2376b8
Tyler Liu <tyler4long@gmail.com>
2024-09-19 17:49:04 -0700

    Add two test cases back

---
+++ test/react.spec.tsx
@@ -0,0 +1,50 @@
+// @vitest-environment jsdom
+import { describe, expect, test } from 'vitest';
+import { render, screen, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+
+import { manage } from '../src';
+import { auto } from '../src/react';
+
+class Store {
+  public count = 0;
+  public increase() {
+    this.count += 1;
+  }
+}
+
+const store = manage(new Store());
+
+const renderHistory: number[] = [];
+
+const App = auto((props: { store: Store }) => {
+  const { store } = props;
+  renderHistory.push(store.count);
+  return (
+    <div>
+      <span role="counter">{store.count}</span>
+      <button onClick={() => store.increase()}>+</button>
+    </div>
+  );
+});
+
+describe('React', () => {
+  test('default', async () => {
+    expect(store.$e.listenerCount()).toBe(0);
+    render(<App store={store} />);
+    expect(store.$e.listenerCount()).toBe(1);
+    const minusButton = screen.getByText('+');
+    await userEvent.click(minusButton);
+    expect(store.$e.listenerCount()).toBe(1);
+    await userEvent.click(minusButton);
+    expect(store.$e.listenerCount()).toBe(1);
+    await userEvent.click(minusButton);
+    expect(store.$e.listenerCount()).toBe(1);
+    const span = await screen.findByRole('counter');
+    expect(parseInt(span.textContent!.trim(), 10)).toBe(store.count);
+    expect(store.count).toBe(3);
+    expect(renderHistory).toEqual([0, 1, 2, 3]);
+    cleanup();
+  });
+});
---
+++ test/react2.spec.tsx
@@ -0,0 +1,77 @@
+// @vitest-environment jsdom
+import { render, screen, cleanup, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { describe, expect, test } from 'vitest';
+
+import { manage } from '../src';
+import { auto } from '../src/react';
+
+class Store {
+  public hanzi: Hanzi;
+  public hanzis = [new Hanzi('刘'), new Hanzi('春'), new Hanzi('涛')];
+  public index = 0;
+
+  public constructor() {
+    this.hanzi = this.hanzis[0];
+  }
+
+  public changeHanzi() {
+    this.index += 1;
+    if (this.index > 2) {
+      this.index = 0;
+    }
+    this.hanzi = this.hanzis[this.index];
+  }
+}
+
+class Hanzi {
+  public hanzi: string;
+  public constructor(hanzi: string) {
+    this.hanzi = hanzi;
+  }
+}
+
+const store = manage(new Store());
+
+const App = auto((props: { store: Store }) => {
+  const { store } = props;
+  return (
+    <>
+      <button onClick={() => store.changeHanzi()}>Change Hanzi</button>
+      <HanziComponent hanzi={store.hanzi} />
+    </>
+  );
+});
+
+const renderHistory: string[] = [];
+const HanziComponent = auto((props: { hanzi: Hanzi }) => {
+  const { hanzi } = props;
+  renderHistory.push(hanzi.hanzi);
+  return hanzi.hanzi;
+});
+
+describe('React', () => {
+  test('default', async () => {
+    render(<App store={store} />);
+    const changeButton = screen.getByText('Change Hanzi');
+    expect(renderHistory).toEqual(['刘']);
+    act(() => {
+      store.hanzi.hanzi = '劉';
+    });
+    expect(renderHistory).toEqual(['刘', '劉']);
+    await userEvent.click(changeButton);
+    expect(renderHistory).toEqual(['刘', '劉', '春']);
+    act(() => {
+      store.hanzi.hanzi = '耀';
+    });
+    expect(renderHistory).toEqual(['刘', '劉', '春', '耀']);
+    await userEvent.click(changeButton);
+    expect(renderHistory).toEqual(['刘', '劉', '春', '耀', '涛']);
+    act(() => {
+      store.hanzi.hanzi = '阳';
+    });
+    expect(renderHistory).toEqual(['刘', '劉', '春', '耀', '涛', '阳']);
+    cleanup();
+  });
+});
