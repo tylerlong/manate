ac59421bc10e06e8887d618e2bc3245789dfaf1e
Tyler Liu <tyler4long@gmail.com>
2024-11-15 13:12:46 -0800

    Rewrite event emitters

--- dev-notes.md
+++ dev-notes.md
@@ -99,3 +99,12 @@ Instead, we could make `position` a property of `monster`.
   - mobx doesn't require you to pass the managed object through props, which is amazing!
 - support computed values
   - Long ago I supported this feature in the SubX project.
+
+## Version 2 Notes
+
+for readEmitter, it is always batched. because we know when to start/stop listener for events.
+We provide a `run` method with a `fn` callback. The run method will return the result of `fn` and the `readCache`. `readCache` records all the read operations happened during the execution of `fn`.
+
+for writeEmitter, by default it is not batched. and we also don't know when to stop listening, it depends on the events and isTrigger.
+We provide a `batch` method with a `fun` callback. During the execution of `fn`, all events are batched/cached. When `fn` finished, all events will be issued.
+If no batch, we emit `WriteEvent`, if bached, we emit `WriteCache`.
--- src/events.ts
+++ src/events.ts
@@ -1,41 +1,105 @@
-export interface ProxyTrapEvent {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export type WriteEvent = {
   target: object;
   prop: PropertyKey;
-}
+};
+export type GetEvent = WriteEvent & {
+  value: any;
+};
+export type HasEvent = WriteEvent & {
+  value: boolean;
+};
+export type KeysEvent = {
+  target: object;
+  value: PropertyKey[];
+};
 
-export class EventEmitter {
-  private _batch = false;
-  private cache: ProxyTrapEvent[] = [];
+type WriteCache = Map<object, Set<PropertyKey>>;
+export class WriteEventEmitter {
+  private cache: WriteCache = new Map();
+  private batchCounter = 0;
 
-  public set batch(value: boolean) {
-    this._batch = value;
-    if (!value && this.cache.length > 0) {
-      this.listeners.forEach((listener) => listener(this.cache));
-      this.cache = [];
+  public batch<T>(f: () => T): T {
+    this.batchCounter++;
+    try {
+      return f();
+    } finally {
+      this.batchCounter--;
+      if (this.batchCounter === 0 && this.cache.size > 0) {
+        this.listeners.forEach((listener) => listener(this.cache));
+        this.cache.clear();
+      }
     }
   }
 
-  /**
-   * @internal
-   */
-  listeners = new Set<(mes: ProxyTrapEvent[]) => void>();
+  listeners = new Set<(e: WriteEvent | WriteCache) => void>();
 
-  on(listener: (mes: ProxyTrapEvent[]) => void) {
+  on(listener: (e: WriteEvent | WriteCache) => void) {
     this.listeners.add(listener);
   }
 
-  off(listener: (mes: ProxyTrapEvent[]) => void) {
+  off(listener: (e: WriteEvent | WriteCache) => void) {
     this.listeners.delete(listener);
   }
 
-  /**
-   * @internal
-   */
-  emit(me: ProxyTrapEvent) {
-    if (this._batch) {
-      this.cache.push(me);
+  emit(me: WriteEvent) {
+    if (this.batchCounter === 0) {
+      this.listeners.forEach((listener) => listener(me));
     } else {
-      this.listeners.forEach((listener) => listener([me]));
+      if (!this.cache.has(me.target)) {
+        this.cache.set(me.target, new Set());
+      }
+      this.cache.get(me.target)!.add(me.prop);
+    }
+  }
+}
+
+type ReadCache = Map<
+  object,
+  {
+    get: { [prop: PropertyKey]: any };
+    has: { [prop: PropertyKey]: boolean };
+    keys?: PropertyKey[];
+  }
+>;
+export class ReadEventEmitter {
+  private caches = new Set<ReadCache>();
+
+  run<T>(f: () => T): [T, ReadCache] {
+    const readCache = new Map();
+    this.caches.add(readCache);
+    try {
+      const r = f();
+      return [r, readCache];
+    } finally {
+      this.caches.delete(readCache);
+    }
+  }
+
+  emitGet(ge: GetEvent) {
+    for (const cache of this.caches.values()) {
+      if (!cache.has(ge.target)) {
+        cache.set(ge.target, { get: {}, has: {} });
+      }
+      cache.get(ge.target)!.get[ge.prop] = ge.value;
+    }
+  }
+
+  emitHas(he: HasEvent) {
+    for (const cache of this.caches.values()) {
+      if (!cache.has(he.target)) {
+        cache.set(he.target, { get: {}, has: {} });
+      }
+      cache.get(he.target)!.has[he.prop] = he.value;
+    }
+  }
+
+  emitKeys(ke: KeysEvent) {
+    for (const cache of this.caches.values()) {
+      if (!cache.has(ke.target)) {
+        cache.set(ke.target, { get: {}, has: {} });
+      }
+      cache.get(ke.target)!.keys = ke.value;
     }
   }
 }
