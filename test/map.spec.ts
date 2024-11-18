4bd6fb0bd9208d58b13f7e6507bd97bdc77a0711
Tyler Liu <tyler4long@gmail.com>
2024-11-18 10:36:35 -0800

    Add more test cases for map

--- test/map.spec.ts
+++ test/map.spec.ts
@@ -37,4 +37,39 @@ describe('map', () => {
     ma.m.set('a', 1); // save value should not trigger autoRun
     expect(count).toBe(1);
   });
+
+  test('autoRun-has', () => {
+    class A {
+      public m = new Map<string, number>();
+    }
+    const a = new A();
+    const ma = manage(a);
+    ma.m.set('a', 1);
+    let count = 0;
+    const runner = autoRun(() => {
+      expect(ma.m.has('a')).toBeDefined();
+      count += 1;
+    });
+    runner.start();
+    ma.m.delete('a');
+    expect(count).toBe(2);
+  });
+
+  test('autoRun-has-unchanged', () => {
+    class A {
+      public m = new Map<string, number>();
+    }
+    const a = new A();
+    const ma = manage(a);
+    ma.m.set('a', 1);
+    let count = 0;
+    const runner = autoRun(() => {
+      expect(ma.m.has('a')).toBeDefined();
+      count += 1;
+    });
+    runner.start();
+    ma.m.set('a', 2);
+    ma.m.delete('b');
+    expect(count).toBe(1);
+  });
 });
