00f7ba5acdabbc05e652bc3fc2b25099fc791098
Tyler Liu <tyler4long@gmail.com>
2024-11-17 20:59:33 -0800

    Add a map test case

--- test/map-and-set.spec.ts
+++ test/map-and-set.spec.ts
@@ -1,6 +1,8 @@
+import { inspect } from 'util';
+
 import { describe, expect, test } from 'vitest';
 
-import { manage } from '../src';
+import { manage, readEmitter } from '../src';
 
 describe('map and set', () => {
   test('map', () => {
@@ -21,5 +23,21 @@ describe('map and set', () => {
     const ma = manage(a);
     ma.s.add('a');
     expect(ma.s.has('a')).toBe(true);
+  });
+  test('traps', () => {
+    class A {
+      public m = new Map<string, number>();
+    }
+    const a = new A();
+    const ma = manage(a);
+    ma.m.set('a', 1);
+    const [, readLogs] = readEmitter.run(() => {
+      expect(ma.m.get('a')).toBe(1);
+      expect(ma.m.has('a')).toBe(true);
+    });
+    expect(inspect(readLogs)).toBe(`Map(2) {
+  A { m: Map(1) { 'a' => 1 } } => { get: { m: [Map] }, has: {} },
+  Map(1) { 'a' => 1 } => { get: { a: 1 }, has: { a: true } }
+}`);
   });
 });
