a190628921033b3dd8c98463225a8c716a7aa508
Tyler Liu <tyler4long@gmail.com>
2024-11-19 10:47:05 -0800

    Test React global store

--- test/react4.spec.tsx
+++ test/react4.spec.tsx
@@ -45,4 +45,30 @@ describe('React', () => {
     expect(renderHistory).toEqual([0, 1, 2]);
     cleanup();
   });
+
+  test('global store', async () => {
+    const App = auto(() => {
+      renderHistory.push(store.count);
+      return (
+        <div>
+          <span role="count">{store.count}</span>
+          <button onClick={() => store.increase()}>+</button>
+        </div>
+      );
+    });
+    store.count = 0;
+    renderHistory.length = 0;
+    render(<App />);
+    act(() => {
+      store.count += 1;
+    });
+    act(() => {
+      store.count += 1;
+    });
+    const span = screen.getByRole('count');
+    expect(parseInt(span.textContent!.trim(), 10)).toBe(store.count);
+    expect(store.count).toBe(2);
+    expect(renderHistory).toEqual([0, 1, 2]);
+    cleanup();
+  });
 });
