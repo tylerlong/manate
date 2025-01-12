acb9b14b6c2df19da37e3766868916b8467a76a3
Tyler Liu <tyler4long@gmail.com>
2025-01-11 23:52:52 -0800

    Upgrade dependencies

---
+++ test/manage-react.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { describe, test } from "vitest";
+
+import { manage } from "../src/index.ts";
+
+describe("Manage React", () => {
+  test("default", () => {
+    const App = <h1>Hello world</h1>;
+    manage({ temp: App });
+  });
+});
--- test/react6.spec.tsx
+++ test/react6.spec.tsx
@@ -1,10 +1,10 @@
 // @vitest-environment jsdom
-import { cleanup, render, screen } from '@testing-library/react';
-import React, { ReactElement } from 'react';
-import { describe, expect, test } from 'vitest';
+import { cleanup, render, screen } from "@testing-library/react";
+import React, { ReactElement } from "react";
+import { describe, expect, test } from "vitest";
 
-import { manage } from '../src';
-import { auto } from '../src/react';
+import { manage } from "../src/index.ts";
+import { auto } from "../src/react.ts";
 
 class Store {
   public count = 0;
@@ -18,8 +18,8 @@ const store = manage(new Store());
 
 const renderHistory: number[] = [];
 
-describe('React', () => {
-  test('ReactElement as props', async () => {
+describe("React", () => {
+  test("ReactElement as props", async () => {
     const App = auto((props: { toolBarItems: (string | ReactElement)[] }) => {
       const { toolBarItems } = props;
       return (
@@ -33,19 +33,20 @@ describe('React', () => {
     render(
       <App
         toolBarItems={[
-          'about',
-          '|',
-          'print',
-          '|',
+          "about",
+          "|",
+          "print",
+          "|",
           <i
             key="preferences-toolbar-item"
             title="Preferences"
             className="fa fa-cog"
-          ></i>,
+          >
+          </i>,
         ]}
       />,
     );
-    const span = await screen.findByRole('count');
+    const span = await screen.findByRole("count");
     expect(parseInt(span.textContent!.trim(), 10)).toBe(5);
     cleanup();
   });
